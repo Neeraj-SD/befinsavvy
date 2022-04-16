26132446e9a1f3aaaa4bdde1b2f2af4cb87eb908
Neeraj-SD <neerajsd04@gmail.com>
2022-04-16 15:49:38 +0530

    task model updated

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Task {
   String id;
   DateTime date;
@@ -7,4 +9,52 @@ class Task {
     required this.date,
     required this.status,
   });
+
+  Task copyWith({
+    String? id,
+    DateTime? date,
+    bool? status,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      date: date ?? this.date,
+      status: status ?? this.status,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'date': date.millisecondsSinceEpoch,
+      'status': status,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'] ?? '',
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
+      status: map['status'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Task.fromJson(String source) => Task.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Task(id: $id, date: $date, status: $status)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Task &&
+        other.id == id &&
+        other.date == date &&
+        other.status == status;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ date.hashCode ^ status.hashCode;
 }
